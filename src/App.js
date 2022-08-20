a60cbd226b820a812477bb99dc0f6f7823839a00
Nhi Truong <tamnhi96@gmail.com>
2022-08-20 02:02:53 -0700

    Divide route for page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Home/Header/Header';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      JIRA_UI
-    </div>
+    <BrowserRouter>  
+    <Header /> 
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
